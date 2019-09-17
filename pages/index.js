d235e9480c814cd42cdbf617a005899550e8f222
Jovan Sutic <jovan.sutic@weblab.co.rs>
2019-09-17 15:06:25 +0200

    add cypress

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const Home = () => {
       <Nav />
 
       <div className="hero">
-        <h1 className="title">ovo je sa test branch-a!</h1>
+        <h1 className="title">Ovo ovde je sa test branch-a!</h1>
         <p className="description">
           To get started, edit the <code>pages/index.js</code> or <code>pages/api/date.js</code> files, then save to reload.
         </p>
