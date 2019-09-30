e7e663492692ac97b6bcfb2838cca263d9cd11cf
Jovan Sutic <jovan.sutic@weblab.co.rs>
2019-09-30 18:48:47 +0200

    under construction page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withCSS(withSass({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -7,4 +10,4 @@ module.exports = {
 
     return config
   }
-}
+}))
