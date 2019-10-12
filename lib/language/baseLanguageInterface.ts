7b0fa680e15ba55f6ca50724641b4aac0a7a5f74
Jovan Sutic <jovan.sutic@weblab.co.rs>
2019-10-13 00:11:42 +0200

    new sections layout

--- lib/language/baseLanguageInterface.ts
+++ lib/language/baseLanguageInterface.ts
@@ -13,10 +13,12 @@ export default interface LanguageInterface {
 	homeTitle: string;
 	homeImg_1: string;
 	homeImg_2: string;
+	homeImg_3: string;
 	homeHeader_1: string;
 	homeHeader_2: string;
 	homeHeader_3: string;
 	homeHeader_4: string;
+	homeHeader_5: string;
 	homeWhatDescribe: string;
 	homeWhatBenTitle: string;
 	homeWhatBen: string;
