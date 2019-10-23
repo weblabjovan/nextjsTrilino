afedea900b95fdc6372fa865b88df611911af2c7
Jovan Sutic <jovan.sutic@weblab.co.rs>
2019-10-23 16:43:10 +0200

    partner login test change

--- cypress/integration/partnerLog_spec.js
+++ cypress/integration/partnerLog_spec.js
@@ -1,7 +1,7 @@
 
 describe('Partner login page layout test', function() {
     beforeEach(()=>{
-        cy.visit('http://localhost:3000/partnershipLogin?language=sr&option=register');
+        cy.visit('http://localhost:3000/partnershipLogin?language=sr&page=register');
     })
 
   it('has all necessary fields', function(){
@@ -12,7 +12,7 @@ describe('Partner login page layout test', function() {
   	cy.get('input[placeholder="Kontakt email"]');
   	cy.get('input[placeholder="Kontakt telefon"]');
   	cy.get('#loginRedirection').click();
-  	cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=sr&option=login');
+  	cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=sr&page=login');
   	cy.get('input[placeholder="Email"]');
   	cy.get('input[placeholder="Lozinka"]');
   })
@@ -20,7 +20,7 @@ describe('Partner login page layout test', function() {
   it('changes the language serbian and english', function(){
   	cy.get('button.btn-secondary').click();
     cy.get('.dropdown-menu button:nth-of-type(2)').click();
-    cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=en&option=register');
+    cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=en&page=register');
   	cy.get('input[placeholder="Venue name"]');
   	cy.get('input[placeholder="Tax identification number"]');
   	cy.get('.logInput div').contains('City');
@@ -28,16 +28,16 @@ describe('Partner login page layout test', function() {
   	cy.get('input[placeholder="Contact email"]');
   	cy.get('input[placeholder="Contact phone"]');
   	cy.get('#loginRedirection').click();
-  	cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=en&option=login');
+  	cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=en&page=login');
   	cy.get('input[placeholder="Email"]');
   	cy.get('input[placeholder="Password"]');
   	cy.get('button.btn-secondary').click();
     cy.get('.dropdown-menu button:nth-of-type(1)').click();
-    cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=sr&option=login');
+    cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=sr&page=login');
     cy.get('input[placeholder="Email"]');
   	cy.get('input[placeholder="Lozinka"]');
   	cy.get('#registrationRedirection').click();
-  	cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=sr&option=register');
+  	cy.url().should('eq', 'http://localhost:3000/partnershipLogin?language=sr&page=register');
   	cy.get('input[placeholder="Naziv lokala"]');
   	cy.get('input[placeholder="PIB"]');
   	cy.get('.logInput div').contains('Grad');
