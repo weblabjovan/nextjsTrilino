3ce58722b0b9f25b7f3632569360497052c84eda
Jovan Sutic <jovan.sutic@weblab.co.rs>
2020-01-18 22:46:45 +0100

    host log

--- lib/helpers/specificAdminFunctions.ts
+++ lib/helpers/specificAdminFunctions.ts
@@ -118,6 +118,7 @@ export const setPhotosForDelete = (photos: Array<object>, photo: string): Array<
 
 export const isDevEnvLogged = async (context: any, ): Promise<boolean> => {
 	const link = setUpLinkBasic({path: context.asPath, host: context.req.headers.host});
+	console.log(link);
 	if (link['host'] === 'dev.trilino') {
 		const allCookies = nextCookie(context);
     	const devAuth = allCookies['trilino-dev-auth'];
