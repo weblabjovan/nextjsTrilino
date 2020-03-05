350a1fcee0df806acb809fbfb605097819f6567e
Jovan Sutic <jovan.sutic@weblab.co.rs>
2020-03-05 12:50:53 +0100

    logger in prod

--- store.js
+++ store.js
@@ -1,17 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import plainMiddleware from './middleware/plainMiddleware';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 
-
-
 export const initializeStore = () => {
+	const middleware = process.env.NODE_ENV === 'production' ? composeWithDevTools(applyMiddleware(thunk)) : composeWithDevTools(applyMiddleware(thunk, logger));
 
-	const middleware = composeWithDevTools(applyMiddleware(thunk, logger));
-	
   return createStore(
     rootReducer,
     middleware
