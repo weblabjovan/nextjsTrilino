fcb42b3ee33365d355c233eeb51b65d9bdf8a036
Jovan Sutic <jovan.sutic@weblab.co.rs>
2020-04-06 17:02:50 +0200

    test change

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -7,18 +7,16 @@ describe('Home page layout test', function() {
     cy.get('button.btn-secondary').click();
     cy.get('.dropdown-menu button:nth-of-type(2)').click();
     cy.url(`http://localhost:3000/?language=en`);
-    cy.get('.footerWrapper ul li:nth-of-type(1)').contains('Login');
-    cy.get('.footerWrapper ul li:nth-of-type(2)').contains('Search');
-    cy.get('.footerWrapper ul li:nth-of-type(3)').contains('Terms of service');
-    cy.get('.footerWrapper ul li:nth-of-type(4)').contains('Partnership');
-    cy.get('.footerWrapper ul li:nth-of-type(5)').contains('Contact');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(1)').contains('Login');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(2)').contains('Search');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(3)').contains('Partnership');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(4)').contains('Contact');
     cy.get('button.btn-secondary').click();
     cy.get('.dropdown-menu button:nth-of-type(1)').click();
     cy.url(`http://localhost:3000/?language=sr`);
-    cy.get('.footerWrapper ul li:nth-of-type(1)').contains('Prijava');
-    cy.get('.footerWrapper ul li:nth-of-type(2)').contains('Pretraga');
-    cy.get('.footerWrapper ul li:nth-of-type(3)').contains('Uslovi korišćenja');
-    cy.get('.footerWrapper ul li:nth-of-type(4)').contains('Saradnja');
-    cy.get('.footerWrapper ul li:nth-of-type(5)').contains('Kontakt');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(1)').contains('Prijava');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(2)').contains('Pretraga');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(3)').contains('Saradnja');
+    cy.get('.footerWrapper ul:nth-of-type(1) li:nth-of-type(4)').contains('Kontakt');
   })
 })
